c94a486df4f0a4aa32b1b508e3c1ea545badd27f
XIN <vialin1128@gmail.com>
2022-05-11 12:59:31 +0800

    实现简单路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import {Link,Route} from 'react-router-dom'
+import About from "./pages/About";
+import Home from "./pages/Home";
 import './pages/css/bootstrap.css'
-import Header from "./components/header";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="row">
-          <Header/>
+          <div className='col-xs-offset-8'>
+            <div className='page-header'><h2>React Router Demo</h2></div>
+          </div>
         </div>
         <div className="row">
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
-              <Link className="list-group-item" to="/about">About</Link>
-              <Link className="list-group-item" to="/home">Home</Link>
+                <Link className="list-group-item" to="/about">About</Link>
+                <Link className="list-group-item" to="/home">Home</Link>
             </div>
           </div>
           <div className="col-xs-6">
             <div className="panel">
               <div className="panel-body">
-                {/* 注册路由 */}
+                  <Route path="/about" component={About}/>
+                  <Route path="/home" component={Home}/>
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
-import About from "./pages/About";
-import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/home" element={<Home />}/>
-      </Routes>
+      <App/>
     </BrowserRouter>
-  </React.StrictMode>
 )
