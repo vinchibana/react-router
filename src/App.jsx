a6f667961991575733ffe1e42a0760cdef4d2347
XIN <vialin1128@gmail.com>
2022-05-11 19:40:18 +0800

    实现二级/嵌套路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react'
-import {Link,Route} from 'react-router-dom'
+import {Redirect, Route, Switch} from 'react-router-dom'
 import About from "./pages/About";
 import Home from "./pages/Home";
 import './pages/css/bootstrap.css'
+import MyNavLink from "./components/MyNavLink";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="row">
-          <div className='col-xs-offset-8'>
+          <div className='col-xs-offset-2'>
             <div className='page-header'><h2>React Router Demo</h2></div>
           </div>
         </div>
         <div className="row">
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
-                <Link className="list-group-item" to="/about">About</Link>
-                <Link className="list-group-item" to="/home">Home</Link>
+              <MyNavLink to='/home'>Home</MyNavLink>
+              <MyNavLink to='/about'>About</MyNavLink>
             </div>
           </div>
           <div className="col-xs-6">
             <div className="panel">
               <div className="panel-body">
+                {/* 路由组件 */}
+                <Switch>
                   <Route path="/about" component={About}/>
                   <Route path="/home" component={Home}/>
+                  <Redirect to="/about" />
+                </Switch>
               </div>
             </div>
           </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from "react";
+import MyNavLink from "../../components/MyNavLink";
+import {Redirect, Route, Switch} from "react-router-dom";
+import News from "./News";
+import Message from "./Message";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h3>Home content</h3>
+        <ul className="nav nav-tabs">
+          <li>
+            <MyNavLink to="/home/news">News</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/home/message">Message</MyNavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/home/news" component={News} />
+          <Route path="/home/message" component={Message} />
+          <Redirect to="/home/news" />
+        </Switch>
       </div>
     );
   }
